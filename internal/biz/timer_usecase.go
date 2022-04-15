f29dd471895a7a9fcde31f04233404c8478b5de7
leslie <leslie@hellotalk.net>
2022-04-15 18:09:56 +0800

    make generate about timer

--- internal/biz/timer_usecase.go
+++ internal/biz/timer_usecase.go
@@ -2,16 +2,31 @@ package biz
 
 import (
 	"context"
+	"github.com/apache/pulsar-client-go/pulsar"
+	"plan-to-remind/internal/conf"
 	"plan-to-remind/internal/data/model"
+	"plan-to-remind/internal/pkg/mq"
+	producer2 "plan-to-remind/internal/pkg/mq/producer"
 	"time"
 )
 
 type TimerUsecase struct {
-	parser *cronParser
+	parser   *cronParser
+	producer mq.Producer
 }
 
-func NewTimerUsecase() *TimerUsecase {
-	return &TimerUsecase{parser: &cronParser{}}
+func NewTimerUsecase(data *conf.Data) *TimerUsecase {
+	cli, err := pulsar.NewClient(pulsar.ClientOptions{
+		URL: data.Pulsar.Url,
+	})
+	if err != nil {
+		panic(err)
+	}
+	producer, err := producer2.NewPulsarProducer(data.Pulsar.Topic, cli)
+	if err != nil {
+		panic(err)
+	}
+	return &TimerUsecase{parser: &cronParser{}, producer: producer}
 }
 
 // UserPlanPush 用户任务推送
--- internal/service/timer.go
+++ internal/service/timer.go
@@ -1,7 +1,15 @@
 package service
 
-import "github.com/go-kratos/kratos/v2/log"
+import (
+	"github.com/go-kratos/kratos/v2/log"
+	"plan-to-remind/internal/biz"
+)
 
 type TimerService struct {
 	log *log.Helper
+	uc  *biz.TimerUsecase
+}
+
+func NewTimerService(logger log.Logger, uc *biz.TimerUsecase) *TimerService {
+	return &TimerService{log: log.NewHelper(logger), uc: uc}
 }
